45a2aa0293348dead14fe7743d130c58c11b71a1
oznchex <114240463+oznchex@users.noreply.github.com>
2025-08-06 15:38:54 +0900

    feat: data.py에 ContentOption(콘텐츠 옵션) 데이터 추가

--- app/models/data.py
+++ app/models/data.py
@@ -17,3 +17,13 @@ class ContentData(BaseModel):
     metadata: Dict[str, Any] = {}
     created_at: datetime
     updated_at: datetime 
+class ContentOptoin(BaseModel):
+    """콘텐츠 옵션 몯레"""
+    content_option_id: int
+    price: float
+    name: str
+    option_type: ContentType
+    description: Optional[str] = None
+    document_file_url: Optional[str] = None
+    document_link_url: Optional[str] = None
+    document_original_file_name: Optional[str] = None
